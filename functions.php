7a449e8dc0c68cb01e9c28f22b4b8a95eca9ea48
The Metaist <metaist@metaist.com>
2014-01-26 22:21:11 -0500

    clean: disabled functions

--- functions.php
+++ functions.php
@@ -1,14 +1,7 @@
 <?php
-
 if (!function_exists('bim_setup')):
 function bim_setup() {
   // add_theme_support('automatic-feed-links');
-  // add_theme_support('custom-header');
-
-  //register_nav_menus( array(
-	//	'primary'   => 'Top primary menu',
-	//	'secondary' => 'Top secondary menu'
-	//));
 
   add_theme_support( 'post-formats', array(
 		//Possible: 'aside', 'image', 'video', 'audio', 'quote', 'link', 'gallery',
@@ -25,44 +18,9 @@ function bim_setup() {
 endif;
 add_action('after_setup_theme', 'bim_setup');
 
-//function bim_widgets_init() {
-//  register_sidebar( array(
-//		'name'          => 'Primary Sidebar',
-//		'id'            => 'sidebar-1',
-//		'description'   => 'Main sidebar that appears on the right.',
-//		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-//		'after_widget'  => '</aside>',
-//		'before_title'  => '<h1 class="widget-title">',
-//		'after_title'   => '</h1>',
-//	));
-//
-//	register_sidebar(array(
-//		'name'          => 'Content Sidebar',
-//		'id'            => 'sidebar-2',
-//		'description'   => 'Additional sidebar that appears on the right.',
-//		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-//		'after_widget'  => '</aside>',
-//		'before_title'  => '<h1 class="widget-title">',
-//		'after_title'   => '</h1>',
-//	));
-//
-//	register_sidebar(array(
-//		'name'          => 'Footer Widget Area',
-//		'id'            => 'sidebar-3',
-//		'description'   => 'Appears in the footer section of the site.',
-//		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-//		'after_widget'  => '</aside>',
-//		'before_title'  => '<h1 class="widget-title">',
-//		'after_title'   => '</h1>',
-//	));
-//}//end function
-//add_action('widgets_init', 'bim_widgets_init');
-
 function bim_title( $title, $sep, $seplocation ) {
   global $page, $paged;
-
-  // Don't affect in feeds.
-  if (is_feed()) { return $title; }
+  if (is_feed()) { return $title; } // Don't change feeds.
 
   // Add the blog name
   if ('right' == $seplocation) {
@@ -83,7 +41,7 @@ function bim_title( $title, $sep, $seplocation ) {
   }
 
   return $title;
-}
+}//end function
 add_filter('wp_title', 'bim_title', 10, 3);
 
 
@@ -91,12 +49,6 @@ function bim_theme_resources() {
 	wp_enqueue_script('lesscss', '//cdnjs.cloudflare.com/ajax/libs/less.js/1.6.1/less.min.js');
 }//end function
 add_action('wp_enqueue_scripts', 'bim_theme_resources');
-
-function bim_audio_resources() {
-  wp_enqueue_style('soundmanager', get_template_directory_uri() . '/lib/soundmanager-2.97/css/inlineplayer.css');
-  wp_enqueue_style('sm-flashblock', get_template_directory_uri() . '/lib/soundmanager-2.97/css/flashblock.css');
-}//end function
-
 
 function bim_get_audio($s) {
   $result = '';
@@ -116,14 +68,14 @@ function bim_get_hebrew_date($t) {
 function bim_get_mimetype($path) {
   $type = '';
   $ext = pathinfo($path, PATHINFO_EXTENSION);
-  if(!empty($ext)) {
-    foreach(wp_get_mime_types() as $exts => $mime) {
-      if(preg_match('!^(' . $exts . ')$!i', $ext)) {
+  if(!empty($ext)):
+    foreach(wp_get_mime_types() as $exts => $mime):
+      if(preg_match('!^(' . $exts . ')$!i', $ext)):
         $type = $mime;
         break;
-      }//end if
-    }//end for each
-  }//end if
+      endif;
+    endforeach;
+  endif;
 
   return $type;
 }//end function
