10425c7cb3b0797ee3594b79329909013d863e2a
metaist <metaist@metaist.com>
2014-06-18 12:24:48 -0400

    add: mincha/ma'ariv warning (6/18)

--- home.php
+++ home.php
@@ -2,6 +2,12 @@
 <div class="container">
   <div class="row">
     <div class="col-xs-12 col-sm-9">
+      <div class="alert alert-warning">
+        Mincha/Ma'ariv today (6/18): Please contact Rabbi Rabinowitz at 781-223-1471.
+      </div>
+    </div>
+  
+    <div class="col-xs-12 col-sm-9">
       <p class="lead"><em>Serving the Jewish community in Malden since 1904.</em></p>
     </div>
     <div class="col-xs-12 col-sm-3 text-muted">
