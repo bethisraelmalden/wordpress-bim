f3262a084b1c56e90f6c4bf61733daf6001faab6
The Metaist <metaist@metaist.com>
2015-02-18 19:21:44 -0500

    update: purim flyer

--- page-rsvp.php
+++ page-rsvp.php
@@ -129,7 +129,7 @@
     </form>
     <div class="col-sm-4 text-center">
       <a href="http://bethisraelmalden.org/2015/02/purim-bash-seudah-5775/">
-        <img src="http://bethisraelmalden.org/wp-content/uploads/2015/02/purim-flyer-300x169.png" />
+        <img src="http://bethisraelmalden.org/wp-content/uploads/2015/02/purim-5775-300x169.png" />
       </a>
     </div>
   </div>
