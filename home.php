1490617a1bb8a1d5ce1dea4243c097dd822c1114
The Metaist <metaist@metaist.com>
2020-03-19 16:25:24 -0400

    update: livestream url

--- home.php
+++ home.php
@@ -20,7 +20,7 @@
   <div class="row">
     <div class="col-xs-12 center-align">
       <div class="embed-responsive embed-responsive-16by9">
-        <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/rwrnj58-F7E" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/live_stream?channel=UCmifAaxDDUILnFheV4VFnHw" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
       </div>
     </div>
   </div>
--- page-live.php
+++ page-live.php
@@ -2,7 +2,7 @@
 
 <div id="content" class="container center-align">
   <div class="embed-responsive embed-responsive-16by9">
-    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/rwrnj58-F7E" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/live_stream?channel=UCmifAaxDDUILnFheV4VFnHw" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
   </div>
 </div>
 
